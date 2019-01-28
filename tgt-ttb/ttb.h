7225ca14bee3d49c1f15a9314622787fd8d9a696
Timothy Trippel <timothy.trippel@ll.mit.edu>
2019-01-28 18:01:22 -0500

    Removed old TTB backend, starting from scratch

--- tgt-ttb/ttb.h
+++ tgt-ttb/ttb.h
@@ -1,36 +1,40 @@
 #ifndef __TTB_HEADER__
 #define __TTB_HEADER__
+
 /*
- * TTB Backend
- *
- * This is a backend for Icarus verilog which generates a dot file with a
- * dependancy graph showing which signals are dependant on which signals
- *
- * Currently the dot file format is not supported. Instead, I output our own
- * format.
- */
-#include <cstdio>
+File:        ttb.h
+Author:      Timothy Trippel
+Affiliation: MIT Lincoln Laboratory
+Description:
+
+This is an Icarus Verilog backend target module that generates a signal
+dependency graph for a given circuit design. The output format is a 
+Graphviz .dot file.
+*/
+
+// Standard Headers
 #include <map>
 #include <string>
-#include <vector>
 
+// IVL API Header
 #include  <ivl_target.h>
+
+// TTB Headers
 #include "ttb_signal.h"
 
-typedef std::map<std::string, TTB_Signal*> sig_map_t;
+using namespace std;
 
-// Cannot be pointers since these signals could be slices
-typedef std::pair<TTB_Signal, TTB_Signal> connection_t;
+// Debugging Switches
+#define DEBUG_PRINTS 		          false
+
+// // Signal name to object map
+// typedef map<string, TTB_Signal*> sig_map_t;
+
+// // Signal to signal connection pair
+// // (Cannot be pointers since these signals could be slices.)
+// typedef pair<TTB_Signal, TTB_Signal> connection_t;
 
 // Gets the output file set in ttb.cc
 FILE* out_file();
-
-
-extern void propagate_sig(TTB_Signal& aff_sig, std::vector<connection_t>& connections);
-extern void propagate_lpm(const ivl_lpm_t lpm, TTB_Signal& aff_sig, std::vector<connection_t>& connections);
-extern void propagate_log(const ivl_net_logic_t logic, TTB_Signal& aff_sig, std::vector<connection_t>& connections);
-
-extern int process_statement(ivl_statement_t stmt, sig_map_t& ffs, std::vector<connection_t>& connections);
-extern void print_connection(const TTB_Signal& aff_sig, const TTB_Signal& sig, std::vector<connection_t>& connections);
 
 #endif
