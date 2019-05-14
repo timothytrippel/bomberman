8874e67181e5b1d8f7f5c538601cf2aa3d19fb22
Tim Trippel <timothy.trippel@ll.mit.edu>
2019-05-14 13:43:01 -0400

    Added support for launching parallel jobs on a SLURM scheduler

--- circuits/run_aes_sims.sh
+++ circuits/run_aes_sims.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #-------------------------------------------------------------------------------
-# Configurations
+# User-Defined Configurations
 #-------------------------------------------------------------------------------
 
 # Design
@@ -17,18 +17,101 @@ OVERWRITE_RESULTS=0
 
 # HDL Sources
 HDL_BASE_DIR="${BASE_DIR}/circuits/${DESIGN}"
-TOP_MODULE='ttb_test_aes_128'
-DESIGN_HDL='aes_128.v round.v table.v counter.v'
-TESTBENCH_HDL='ttb_test_aes_128.v lfsr.v'
 
 # Trojan Types
-# TROJAN_TYPES='cdd'
-TROJAN_TYPES='cdd cdn cnd cnn ddd ddn dnd dnn tjfree'
+TROJAN_TYPES='cdd'
+# TROJAN_TYPES='cdd cdn cnd cnn ddd ddn dnd dnn tjfree'
 
 # Test Range
-NUM_TEST_INCREMENT=1000
-NUM_TESTS_RANGE=10000001
-# NUM_TESTS_RANGE=10000001
+NUM_TEST_INCREMENT=10
+NUM_TESTS_RANGE=11
+# NUM_TEST_INCREMENT=1000
+# NUM_TESTS_RANGE=100001
+
+# Flags
+INTERACTIVE=0
+
+#-------------------------------------------------------------------------------
+# System Configurations (DO NOT EDIT)
+#-------------------------------------------------------------------------------
+SLURM_SCRIPTS_DIR="${BASE_DIR}/circuits/scripts"
+
+#-------------------------------------------------------------------------------
+# Functions
+#-------------------------------------------------------------------------------
+
+create_output_dir() {
+	if [[ (-d "$OUTPUT_DIR") && (! $OVERWRITE_RESULTS) ]]; then
+		echo "ERROR: output directory ${OUTPUT_DIR} already exists... abort."
+		exit
+	else
+		mkdir -p ${OUTPUT_DIR}
+	fi
+}
+
+gen_dot() {
+	if [[ $INTERACTIVE -eq 1 ]]; then
+
+		# Run job interactively
+		source ${SLURM_SCRIPTS_DIR}/gen_dot.sbatch
+	else
+
+		# Run job non-interactively
+		sbatch \
+			-D ${HDL_BASE_DIR}/${TTYPE} \
+			--export=OUTPUT_DIR=${OUTPUT_DIR},DESIGN=${DESIGN},TTYPE=${TTYPE},CLK_BASENAME=${CLK_BASENAME} \
+			${SLURM_SCRIPTS_DIR}/gen_dot.sbatch > /dev/null
+	fi	
+}
+
+gen_vvp() {
+	if [[ $INTERACTIVE -eq 1 ]]; then
+
+		# Run job interactively
+		source ${SLURM_SCRIPTS_DIR}/gen_vvp.sbatch
+	else
+
+		# Run job non-interactively
+		VVP_JOB_ID=$(\
+			sbatch \
+			-D ${HDL_BASE_DIR}/${TTYPE} \
+			--export=OUTPUT_DIR=${OUTPUT_DIR},DESIGN=${DESIGN},TTYPE=${TTYPE} \
+			${SLURM_SCRIPTS_DIR}/gen_vvp.sbatch)
+	fi	
+}
+
+gen_vcd() {
+	if [[ $INTERACTIVE -eq 1 ]]; then
+
+		# Run job interactively
+		source ${SLURM_SCRIPTS_DIR}/gen_vcd.sbatch
+	else
+
+		# Run job non-interactively
+		VCD_JOB_ID=$(\
+			sbatch \
+				-D ${HDL_BASE_DIR}/${TTYPE} \
+				--dependency=afterok:${VVP_JOB_ID##* } \
+				--export=OUTPUT_DIR=${OUTPUT_DIR},DESIGN=${DESIGN},TTYPE=${TTYPE},NUM_TESTS=${NUM_TESTS} \
+				${SLURM_SCRIPTS_DIR}/gen_vcd.sbatch)
+	fi	
+}
+
+analyze_counters() {
+	if [[ $INTERACTIVE -eq 1 ]]; then
+
+		# Run job interactively
+		source ${SLURM_SCRIPTS_DIR}/analyze_counters.sbatch
+	else
+
+		# Run job non-interactively
+		sbatch \
+			-D ${HDL_BASE_DIR}/${TTYPE} \
+			--dependency=afterok:${VCD_JOB_ID##* } \
+			--export=OUTPUT_DIR=${OUTPUT_DIR},DESIGN=${DESIGN},TTYPE=${TTYPE},NUM_TESTS=${NUM_TESTS} \
+			${SLURM_SCRIPTS_DIR}/analyze_counters.sbatch > /dev/null
+	fi
+}
 
 #-------------------------------------------------------------------------------
 # Launch tests
@@ -36,69 +119,25 @@ NUM_TESTS_RANGE=10000001
 
 # Create Output Directory
 echo "Creating output directory..."
-if [[ (-d "$OUTPUT_DIR") && (! $OVERWRITE_RESULTS) ]]; then
-	echo "ERROR: output directory ${OUTPUT_DIR} already exists... abort."
-	exit
-else
-	mkdir -p ${OUTPUT_DIR}
-fi
+create_output_dir
 
 # Iterate over trojan types 
 for TTYPE in ${TROJAN_TYPES}; do
 
-	echo "Starting analysis of ${TTYPE} Trojans..."
+	# Generate DOT file and VVP simulation binary
+	echo "Starting CFG analysis of ${TTYPE} Trojans..."
+	gen_dot
+	# gen_vvp
 
-	# Set static analysis log file
-	STA_LOG_FILE=${OUTPUT_DIR}/${DESIGN}.${TTYPE}.log
+	# # Launch test bench simulations
+	# for (( NUM_TESTS=${NUM_TEST_INCREMENT}; NUM_TESTS<=${NUM_TESTS_RANGE}; NUM_TESTS += ${NUM_TEST_INCREMENT} )); do	
 
-	echo "Design:      ${DESIGN}" >  ${STA_LOG_FILE}
-	echo "Trojan Type: ${TTYPE}"  >> ${STA_LOG_FILE}
+	# 	# Generate VCD file and analyze for counters
+	# 	echo "Starting simulation with ${NUM_TESTS} tests ..."
+	# 	gen_vcd
+	# 	analyze_counters
 
-	# Switch to HDL directory
-	pushd ${HDL_BASE_DIR}/${TTYPE} > /dev/null
-
-	# Launch DOT file generation
-	echo "Generating DOT graph..." >> ${STA_LOG_FILE}
-	(time iverilog \
-		-o ${DESIGN}-${TTYPE}.dot \
-		-pclk=${CLK_BASENAME} \
-		-t ttb \
-		-s ${TOP_MODULE} \
-		${TESTBENCH_HDL} \
-		${DESIGN_HDL}) >> ${STA_LOG_FILE} 2>&1
-
-	# Launch VVP file generation
-	echo "Generating VVP simulation binary..." >> ${STA_LOG_FILE}
-	(time iverilog \
-		-t vvp \
-		-o ${DESIGN}-${TTYPE}.vvp \
-		${DESIGN_HDL} \
-		${TESTBENCH_HDL}) >> ${STA_LOG_FILE} 2>&1
-
-	# Launch test bench simulations
-	for (( NUM_TESTS=${NUM_TEST_INCREMENT}; NUM_TESTS<=${NUM_TESTS_RANGE}; NUM_TESTS += ${NUM_TEST_INCREMENT} )); do	
-
-		echo "Starting simulation with ${NUM_TESTS} tests ..."
-
-		# Set dynamic analysis log file
-		DNA_LOG_FILE=${OUTPUT_DIR}/${DESIGN}.${TTYPE}.${NUM_TESTS}.log
-
-		# Launch VCD file generation
-		echo "Generating VCD file with ${NUM_TESTS} test cases..." >> ${DNA_LOG_FILE}
-		(time vvp \
-			${DESIGN}-${TTYPE}.vvp \
-			+num_tests=${NUM_TESTS}) >> ${DNA_LOG_FILE} 2>&1
-
-		# Launch counter analysis
-		echo "Launching counter analysis..." >> ${DNA_LOG_FILE}
-		(time pypy \
-			${BASE_DIR}/scripts/analyze.py \
-			${DESIGN}-${TTYPE}.dot \
-			${DESIGN}-${TTYPE}.vcd) >> ${DNA_LOG_FILE} 2>&1
-	done
-
-	# Switch back to top directory
-	popd > /dev/null
+	# done
 
 done
 echo "Done."
