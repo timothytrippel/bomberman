65d488fb06c672c5bdbdf4ddd73c1b768c5a1969
Timothy Trippel <timothy.trippel@ll.mit.edu>
2019-06-04 16:35:11 -0400

    Partially parameterized OR1200 testbench to run multiple programs in series.

--- circuits/or1200/cdn/cores/bench/test-defines.v
+++
@@ -1,8 +0,0 @@
-`define RTL_SIM
-//`define SIMULATOR_MODELSIM
-`define TEST_NAME_STRING "or1200-cdn"
-`define VCD
-`define VCD_DEPTH 0 // 0 = all
-`define VCD_PATH orpsoc_testbench.dut
-// `define PROCESSOR_MONITOR_ENABLE_LOGS
-// `define END_TIME 1000000
--- circuits/or1200/cnn/cores/or1200/counter.v
+++
@@ -1,8 +0,0 @@
-module counter(input wire clk, input wire [127:0] inc, output wire[127:0] count);
-  reg [127:0] counter = 128'd0;
-  assign count = counter;
-
-  always @(posedge clk) begin
-    counter <= counter + inc;
-  end
-endmodule
